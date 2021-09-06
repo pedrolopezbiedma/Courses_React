d40a26c811195a52e1b6d1e03b334d57878fca64
Pedro Lopez <Pedro.Lopez@ngahr.com>
2021-09-06 18:20:28 +0200

    Course Lesson 13

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -7,14 +8,15 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }        
     ]);
 
+    const handleDelete = (blogId) => {
+        const newBlogs = blogs.filter(blog => blog.id !== blogId)
+        setBlogs(newBlogs)
+    }
+
     return ( 
         <div className="home">
-            { blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by: {blog.author}</p>
-                </div>
-            )) }
+            <BlogList blogs={blogs} title='All blogs' handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter((blog) => blog.author == 'mario')} title='Mario blogs' handleDelete={handleDelete} />
         </div>
     );
 }
